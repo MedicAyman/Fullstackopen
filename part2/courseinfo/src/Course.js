d2b308193472e31622c898214fa634b7c389c4f8
Ayman Jaaouani <ayman.jaaouani.aj@gmail.com>
2022-04-25 15:57:05 +0300

    2.2: Course information step7

--- part2/courseinfo/src/Course.js
+++ part2/courseinfo/src/Course.js
@@ -1,11 +1,25 @@
 import React from "react";
 
-export default function Course({course}) {
-  return <div>
+export default function Course({ course }) {
+
+
+  let totalExercises = course.parts.reduce((sum, part) => {
+    return sum += part.exercises
+  }, 0)
+ 
+  
+  return (
+    <div>
       <h1>{course.name}</h1>
 
       <ul>
-        {course.parts.map(part => <li key={part.id}>{part.name}: {part.exercises}</li>)}
+        {course.parts.map((part) => (
+          <li key={part.id}>
+            {part.name}: {part.exercises}
+          </li>
+        ))}
       </ul>
-  </div>
+      <b>total of {totalExercises} exercises</b>
+    </div>
+  );
 }
