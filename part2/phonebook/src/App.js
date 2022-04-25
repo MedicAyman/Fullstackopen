74aa545ddbab3133b5f83a0df1dc2363b3fb9971
Ayman Jaaouani <ayman.jaaouani.aj@gmail.com>
2022-04-26 02:19:17 +0300

    2.8: The Phonebook Step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,24 +1,30 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto" }]);
+  const [persons, setPersons] = useState([{ name: "Arto", number: '0740263097' }]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
-  const handleChange = (event) => {
+  const handleChangeName = (event) => {
     setNewName(event.target.value);
+  };
+  const handleChangeNumber = (event) => {
+    setNewNumber(event.target.value);
   };
 
 
   const addName = (event) => {
     event.preventDefault();
-    if (persons.map(person => person.name).includes(newName)) {
+    if (persons.map(person => person.name).includes(newName) || persons.map(person => person.number).includes(newNumber)) {
       window.alert(`${newName} is already added to phonebook`);
     } else {
       let p = {
         name: newName,
+        number: newNumber
       };
       setPersons(persons.concat(p));
       setNewName("");
+      setNewNumber("")
     }
   };
 
@@ -26,10 +32,14 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>debug: {newName}</div>
+      <div>debug name: {newName}</div>
+      <div>debug number: {newNumber}</div>
       <form onSubmit={addName}>
         <div>
-          name: <input value={newName} onChange={handleChange} />
+          name: <input value={newName} onChange={handleChangeName} />
+        </div>
+        <div>
+          name: <input value={newNumber} onChange={handleChangeNumber} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -38,7 +48,7 @@ const App = () => {
       <h2>Numbers</h2>
       <ul>
         {persons.map((p) => (
-          <li key={p.name + Math.random()}>{p.name}</li>
+          <li key={p.name + Math.random()}>{p.name}: {p.number}</li>
         ))}
       </ul>
     </div>
