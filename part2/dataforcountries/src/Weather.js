cac706508e05bd577d4979fe3754764645bddebe
Ayman Jaaouani <ayman.jaaouani.aj@gmail.com>
2022-04-27 18:32:25 +0300

    2.14*: Data for countries, step3

--- part2/dataforcountries/src/Weather.js
+++ part2/dataforcountries/src/Weather.js
@@ -6,20 +6,25 @@ const api_key = process.env.REACT_APP_API_KEY;
 
 export default function Weather({ country }) {
   const [weather, setWeather] = useState({});
-  let countryCapital= country.capital;
+
+  let countryCapital = country.capital;
   let api_url = `https://api.openweathermap.org/data/2.5/weather?q=${countryCapital}&appid=${api_key}&units=metric`;
-  console.log(api_url)
-  let weather_icon_url = `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`
+  console.log(api_url);
+ let weather_icon_url = `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`;
+
   useEffect(() => {
     axios.get(api_url).then((response) => {
       setWeather(response.data);
-      console.log('response', response.data)
+      console.log("data: ", weather);
+      
     });
   }, []);
 
-  return <div>
+  return (
+    <div>
       <p>Temperature: {weather.main.temp} Celsius</p>
-       <img src={weather_icon_url} />
+      <img src={weather_icon_url} />
       <p>Wind speed: {weather.wind.speed} m/s</p>
-  </div>;
+    </div>
+  );
 }
