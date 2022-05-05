22e8226abd284106400ce19c0474bcaf0fbdfedd
Ayman Jaaouani <ayman.jaaouani.aj@gmail.com>
2022-05-05 23:37:21 +0300

    3.6: Phonebook backend step6

--- part3/PhoneBook-backend/index.js
+++ part3/PhoneBook-backend/index.js
@@ -27,59 +27,60 @@ let persons = [
 app.use(express.json());
 
 app.get("/api/persons", (req, res) => {
-    res.json(persons)
+  res.json(persons);
 });
 
-app.get('/info', (req, res) => {
-    res.type('html')
-    res.send(`<p> there are ${persons.length} on the Phonebook now.</p> <p> ${new Date()} </p>`)
-})
+app.get("/info", (req, res) => {
+  res.type("html");
+  res.send(
+    `<p> there are ${
+      persons.length
+    } on the Phonebook now.</p> <p> ${new Date()} </p>`
+  );
+});
 
-app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(person => person.id === id)
-    res.json(person)
-})
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
+  res.json(person);
+});
 
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
-    res.status(204).end()
-  })
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((person) => person.id !== id);
 
-  const generateId = () => {
-    const maxId = persons.length > 0
-      ? Math.max(...persons.map(n => n.id))
-      : 0
-    return maxId + 1
+  res.status(204).end();
+});
+
+const generateId = () => {
+  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+  console.log("request body: ", req.body);
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: "content or number is missing",
+    });
   }
+  if (persons.find((p) => p.name.toUpperCase() === body.name.toUpperCase())) {
+    return res.status(400).json({
+      error: "Name must be unique",
+    });
+  }
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  };
+  persons = persons.concat(person);
 
-  app.post('/api/persons', (req, res) => {
-    const body = req.body
-    console.log('request body: ', req.body)
-    if (!body.name) {
-      return res.status(400).json({ 
-        error: 'content missing' 
-      })
-    }
+  res.json(person);
+});
 
-    const person = {
-        name: body.name,
-        number: body.number,
-        id: generateId(),
-      }
-      persons = persons.concat(person)
-    
-      res.json(person)
-  })
-     
-
-
-
-
-
-const PORT = 3001
+const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
