114a3303783e4a26d6eed792a66aee8e781e1f7b
Ayman Jaaouani <ayman.jaaouani.aj@gmail.com>
2022-05-06 20:19:22 +0300

    3.17*: Phonebook database, step5

--- part3/PhoneBook-backend/index.js
+++ part3/PhoneBook-backend/index.js
@@ -79,6 +79,21 @@ app.post("/api/persons", (req, res) => {
   });
 });
 
+app.put("/api/persons/:id", (req, res, next) => {
+  const body = req.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      res.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
