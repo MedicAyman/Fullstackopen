590a566c5443d96c82af3c6fdd5ce615a5855eb6
Ayman Jaaouani <ayman.jaaouani.aj@gmail.com>
2022-05-06 19:58:44 +0300

    3.15: Phonebook database, step3

--- part3/PhoneBook-backend/index.js
+++ part3/PhoneBook-backend/index.js
@@ -16,10 +16,18 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
+const errorHandler = (error, req, res, next) => {
+  if (error.name === "CastError")
+    return res.status(400).send({ error: "Malformatted id" });
+
+  next(error);
+};
+app.use(errorHandler)
+
 app.get("/api/persons", (req, res) => {
-  Person.find({}).then(persons => {
-    res.json(persons)
-  })
+  Person.find({}).then((persons) => {
+    res.json(persons);
+  });
 });
 
 app.get("/info", (req, res) => {
@@ -37,11 +45,10 @@ app.get("/api/persons/:id", (req, res) => {
   res.json(person);
 });
 
-app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter((person) => person.id !== id);
-
-  res.status(204).end();
+app.delete("/api/persons/:id", (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id).then(result => {
+    res.status(204).end()
+  }).catch(error => next(error))
 });
 
 const generateId = () => {
@@ -51,26 +58,21 @@ const generateId = () => {
 
 app.post("/api/persons", (req, res) => {
   const body = req.body;
- 
+
   if (!body.name || !body.number) {
     return res.status(400).json({
       error: "content or number is missing",
     });
   }
 
-  /* if (persons.find((p) => p.name.toUpperCase() === body.name.toUpperCase())) {
-    return res.status(400).json({
-      error: "Name must be unique",
-    });
-  } */
   const person = new Person({
     name: body.name,
-    number: body.number
-  })
+    number: body.number,
+  });
 
-  person.save().then(savedPerson => {
-    res.json(savedPerson)
-  })
+  person.save().then((savedPerson) => {
+    res.json(savedPerson);
+  });
 });
 
 const PORT = process.env.PORT || 3001;
