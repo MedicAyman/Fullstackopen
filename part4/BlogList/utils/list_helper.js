c42dca983557b76926b9dea7ddab4cb222b73825
Ayman Jaaouani <ayman.jaaouani.aj@gmail.com>
2022-05-07 19:26:18 +0300

    4.5*: helper functions and unit tests, step3

--- part4/BlogList/utils/list_helper.js
+++ part4/BlogList/utils/list_helper.js
@@ -1,7 +1,14 @@
 const totalLikes = (blogs) => {
-  const totalLike = blogs.reduce((previousValue, currentValue) => {});
+  let likes = blogs.reduce((total, blog) => total + blog.likes, 0);
+  return likes;
+};
+
+const favoriteBlog = (blogs) => {
+  let highest_amount_of_likes = Math.max(...blogs.map((blog) => blog.likes));
+  return blogs.find((blog) => blog.likes === highest_amount_of_likes);
 };
 
 module.exports = {
-  dummy,
+  totalLikes,
+  favoriteBlog,
 };
