3b64ebe72b1574f983acb1874de6253d67a2143f
Ayman Jaaouani <ayman.jaaouani.aj@gmail.com>
2022-05-09 00:21:29 +0300

    4.8: Blog list tests, step1

---
+++ part4/BlogList/tests/test_helper.js
@@ -0,0 +1,18 @@
+const initialBlogs = [
+  {
+    author: "John Doe",
+    title: "First post",
+    url: "wwww.google.com",
+    upvotes: "10",
+  },
+  {
+    author: "Amber heard",
+    title: "How to fake violance",
+    url: "wwww.washingtonpost.com",
+    upvotes: "0",
+  },
+];
+
+module.exports = {
+  initialBlogs,
+};
--- part4/BlogList/utils/config.js
+++ part4/BlogList/utils/config.js
@@ -1,9 +1,13 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const { PORT } = process.env
-const { MONGODB_URI } = process.env
-
+const PORT = process.env.PORT;
+const MONGODB_URI =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI;
+//nbTEXH5emGMUi64e
+//BlogListTest
 module.exports = {
-    PORT,
-    MONGODB_URI,
-}
+  PORT,
+  MONGODB_URI,
+};
