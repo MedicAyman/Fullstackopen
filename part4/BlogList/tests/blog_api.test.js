f3b8f4ef92db6d3d3d2fb273349961e637070a2c
Ayman Jaaouani <ayman.jaaouani.aj@gmail.com>
2022-05-09 00:35:26 +0300

    4.9*: Blog list tests, step2

--- part4/BlogList/tests/blog_api.test.js
+++ part4/BlogList/tests/blog_api.test.js
@@ -1,3 +1,4 @@
+const { transform } = require("lodash");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
@@ -7,7 +8,13 @@ const helper = require("./test_helper");
 
 beforeEach(async () => {
   await Blog.deleteMany({});
-  await Blog.insertMany(helper.initialBlogs);
+  await Blog.insertMany(helper.initialBlogs, "toJSON", {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id;
+      delete returnedObject._id;
+      delete returnedObject.__v;
+    },
+  });
 }, 100000);
 
 test("blogs are returned as json", async () => {
@@ -19,8 +26,14 @@ test("blogs are returned as json", async () => {
 
 test("all blogs are returned", async () => {
   const response = await api.get("/api/blogs");
-
+  console.log("response", response.body[0]);
   expect(response.body).toHaveLength(helper.initialBlogs.length);
+});
+
+test("check if blog has property id", async () => {
+  const response = await api.get("/api/blogs");
+  console.log("ID", response.body[0]);
+  expect(response.body[0].id).toBeDefined();
 });
 
 afterAll(() => {
