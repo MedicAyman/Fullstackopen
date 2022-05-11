33f46aedde622fd52827d10e886a294490e3823f
Ayman Jaaouani <ayman.jaaouani.aj@gmail.com>
2022-05-12 00:47:33 +0300

    5.8: Blog list frontend, step8

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,12 @@
-import React, { useState } from "react";
-
+import React, { useEffect, useState } from "react";
+import blogServices from "../services/blog";
 const Blog = ({ blog }) => {
   const [visible, setVisible] = useState(false);
+  const [likes, setLikes] = useState(null);
+
+  useEffect(() => {
+    setLikes(blog.likes);
+  }, [blog.likes]);
   const hideWhenVisible = { display: visible ? "none" : "" };
   const showWhenVisible = { display: visible ? "" : "none" };
   const toggleVisibility = () => {
@@ -15,16 +20,27 @@ const Blog = ({ blog }) => {
     marginBottom: 5,
   };
 
+  const addLike = () => {
+    setLikes((+likes + 1).toString());
+    blogServices.update({
+      ...blog,
+      likes: likes,
+    });
+  };
   return (
     <div style={blogStyle}>
       <p style={hideWhenVisible}>
-        {blog.title} <button onClick={toggleVisibility}>View</button>
+        {blog.title} <button onClick={toggleVisibility}>View</button>{" "}
+        <button onClick={addLike}>Like</button>
       </p>
       <div style={showWhenVisible}>
         <p>{blog.title}</p>
         <p>Author: {blog.author} </p>
         <p>Url: {blog.url}</p>
-        <p>Likes: {blog.likes}</p>
+        <p>Likes: {likes}</p>
+        <p>
+          <button onClick={addLike}>Like</button>
+        </p>
         <button onClick={toggleVisibility}>Hide</button>
       </div>
     </div>
--- part5/bloglist-frontend/src/services/blog.js
+++ part5/bloglist-frontend/src/services/blog.js
@@ -10,6 +10,10 @@ const getAll = async () => {
   const response = await axios.get(baseUrl);
   return response.data;
 };
+const update = async (newObject) => {
+  const response = await axios.put(`${baseUrl}/${newObject.id}`, newObject);
+  return response.data;
+};
 
 const create = async (newObject) => {
   const config = {
@@ -19,4 +23,4 @@ const create = async (newObject) => {
   return response.data;
 };
 // eslint-disable-next-line import/no-anonymous-default-export
-export default { getAll, create, setToken };
+export default { getAll, create, setToken, update };
