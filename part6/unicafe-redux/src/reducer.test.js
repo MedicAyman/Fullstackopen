3dfe0d170cf6e41980e5bbe501b4c23d145099a4
Ayman Jaaouani <ayman.jaaouani.aj@gmail.com>
2022-05-13 18:36:05 +0300

    6.1: unicafe revisited, step1

--- part6/unicafe-redux/src/reducer.test.js
+++ part6/unicafe-redux/src/reducer.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import deepFreeze from 'deep-freeze'
 import counterReducer from './reducer'
 
@@ -28,6 +29,51 @@ describe('unicafe reducer', () => {
     const newState = counterReducer(state, action)
     expect(newState).toEqual({
       good: 1,
+      ok: 0,
+      bad: 0
+    })
+  })
+
+  test('OK is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('BAD is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('ZERO makes them all 0', () => {
+    const action = {
+      type: 'ZERO'
+    }
+    const state = {...initialState, good: 1}
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
       ok: 0,
       bad: 0
     })
